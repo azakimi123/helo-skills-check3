a8f411a723bd2b64cf6dfe34976ba925fca54d41
azakimi123 <aaron.zakimi@gmail.com>
2020-07-10 14:34:26 -0600

    inital db setup

--- server/index.js
+++ server/index.js
@@ -1,4 +1,8 @@
+require('dotenv').config();
+//requiring different packages and files to run server and database
 const express = require('express'),
+      massive = require('massive'),
+      {SERVER_PORT, CONNECTION_STRING, SESSION_SECRET} = process.env,
       app = express(),
       ctrl = require('./controller');
 
@@ -6,4 +10,14 @@ const express = require('express'),
 
 app.use(express.json());
 
-app.listen(4040, () => console.log('server running on 4040'))
+
+
+//massive set up
+massive({
+    connectionString: CONNECTION_STRING,
+    ssl: {rejectUnauthorized: false}
+}).then(db => {
+    app.set('db', db);
+    console.log('db connected')
+})
+app.listen(SERVER_PORT, () => console.log(`server is running on ${SERVER_PORT}`))
