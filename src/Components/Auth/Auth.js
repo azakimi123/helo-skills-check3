adfa1163e13e2c5e4af53887fc2b3ecdf6bdd9a8
azakimi123 <aaron.zakimi@gmail.com>
2020-07-10 13:46:55 -0600

    react setup complete

---
+++ src/Components/Auth/Auth.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+
+class Auth extends Component{
+    render(){
+        return(
+            <div>
+                <p>Auth</p>
+            </div>
+        )
+    }
+}
+
+
+export default Auth;
