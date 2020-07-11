93518bca62e15dd9e23e9ee73d41f3fa167184a7
azakimi123 <aaron.zakimi@gmail.com>
2020-07-11 16:50:41 -0600

    styled mapped post (test data)

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,12 +1,55 @@
 import React, {Component} from 'react';
+import Post from '../Post/Post';
 import './Dashboard.css';
 
 
 class Dashboard extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            posts: [
+                {
+                    title: 'Naruto',
+                    img: 'https://img1.looper.com/img/gallery/the-entire-naruto-storyline-finally-explained/intro-1583346246.jpg',
+                    content: 'Naruto is awesome, rasengan'
+                },
+                {
+                    title: 'Kakashi Sensei',
+                    img: 'https://www.syfy.com/sites/syfy/files/styles/1200x680/public/wire/legacy/KakashiHatake_Naruto_0.png',
+                    content: 'Kakashi is awesome, chidori and kamui'
+                },
+                {
+                    title: 'Sasuke',
+                    img: 'https://www.dhresource.com/0x0/f2/albu/g9/M00/03/D9/rBVaWF33STWAbHzrAAbKy45W4I4388.jpg',
+                    content: 'Sasuke is awesome, amaterasu, mangekyo'
+                }
+                
+            ],
+            search: '',
+            userPosts: true
+        }
+    }
     render(){
+        let postList = this.state.posts.map((post, index) => (
+            <Post post={post} key={index}/>
+        ))
+        console.log(postList)
         return(
-            <div className='container'>
-                <p>Dashboard</p>
+            <div>
+                <section className='input-container'>
+                    <input
+                        placeholder='Search by Title'
+                        value={this.state.search}/>
+                    <button>Search</button>
+                    <button>Reset</button>
+                    <span>My Posts</span>
+                    <input 
+                        type="checkbox" 
+                        value={this.state.userPosts}/>
+                </section>
+                <section className='post-container'>
+                    <span>{postList}</span>
+                </section>
             </div>
         )
     }
--- src/Components/Post/Post.js
+++ src/Components/Post/Post.js
@@ -1,11 +1,19 @@
 import React, {Component} from 'react';
+import './Post.css';
 
 
 class Post extends Component{
     render(){
+        console.log(this.props.post)
         return(
             <div>
-                <p>Post</p>
+                <section>
+                    <section className='post-card'>
+                        <h2  className='post-title'>{this.props.post.title}</h2>
+                        <img src={this.props.post.img} alt={this.props.post.title} className='post-img'/>
+                        <h3  className='post-content'>{this.props.post.content}</h3>
+                    </section>
+                </section>
             </div>
         )
     }
