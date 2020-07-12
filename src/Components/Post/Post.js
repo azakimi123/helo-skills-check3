5af40cb0c0ef628af21da1b30cb1b770bd8cfec6
azakimi123 <aaron.zakimi@gmail.com>
2020-07-11 22:53:42 -0600

    style adjustments

--- src/Components/Post/Post.js
+++ src/Components/Post/Post.js
@@ -3,15 +3,27 @@ import './Post.css';
 
 
 class Post extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: '',
+            img: '',
+            content: '',
+            author: 'aaron6',
+            authorPic: 'https://image.flaticon.com/icons/svg/2948/2948218.svg'
+        }
+    }
     render(){
         console.log(this.props.post)
         return(
             <div>
-                <section>
+                <section >
                     <section className='post-card'>
                         <h2  className='post-title'>{this.props.post.title}</h2>
-                        <img src={this.props.post.img} alt={this.props.post.title} className='post-img'/>
-                        <h3  className='post-content'>{this.props.post.content}</h3>
+                        <section  className='right-post-card'>
+                            <h3  className='post-auth'>by {this.state.author}</h3>
+                            <img src={this.state.authorPic} alt={this.state.author} className='post-img'/>
+                        </section>
                     </section>
                 </section>
             </div>
